10191f8e87624f54cebcb562f60c333acb9b9915
Dragon <aleha2552654@gmail.com>
2025-09-24 16:53:34 +0300

    Init

---
+++ CollegeApp/Services/CollegeAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApp.Services
+{
+    public class CollegeAppDbContext : IdentityDbContext
+    {
+        public CollegeAppDbContext(DbContextOptions<CollegeAppDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
